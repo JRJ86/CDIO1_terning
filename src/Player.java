517c0a66a01b7c05cad83907965a3ce47a631788
JRJ86 <42966158+JRJ86@users.noreply.github.com>
2018-10-09 18:37:18 +0200

    Filip - player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,19 @@
 public class Player {
+
+    int point = 0;
+
+    public void addPoints(int pointsToAdd){
+
+        point += pointsToAdd;
+
+    }
+
+    public int getPoints(){
+
+        return point;
+
+    }
+
+
+
 }
