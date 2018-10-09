cb826ef25471c200a965655f99821728289ba15b
Jacob <Jacob_ziska@hotmail.com>
2018-10-09 19:21:06 +0200

    Chua's message - finds a winner of the game, when the points reaches above 40... if both player has equal points, they both win. Sorry for the harsh language hihi

--- src/Game.java
+++ src/Game.java
@@ -7,25 +7,51 @@ public class Game {
         Player p1 = new Player();
         Player p2 = new Player();
 
-        System.out.println(p1.getPoints());
+        while (p1.getPoints()<40 && p2.getPoints()<40) {
+            System.out.println();
+            //Player 1
+            System.out.println("Current points for player one: "+p1.getPoints());
 
 
-        d1.roll();
-        d2.roll();
+            d1.roll();
+            d2.roll();
 
-        p1.addPoints(d1.getFaceValue()+d2.getFaceValue());
-
-
-        System.out.println(d1.getFaceValue()+ " og " +d2.getFaceValue());
+            p1.addPoints(d1.getFaceValue() + d2.getFaceValue());
 
 
-        System.out.println(p1.getPoints());
+            System.out.println("Dice rolled: "+d1.getFaceValue() + " and " + d2.getFaceValue());
 
 
+            System.out.println("Points after roll for player one: "+p1.getPoints());
+
+            //Player 2
+            System.out.println();
+            System.out.println("Current points for player two: "+p2.getPoints());
 
 
+            d1.roll();
+            d2.roll();
+
+            p2.addPoints(d1.getFaceValue() + d2.getFaceValue());
 
 
+            System.out.println("Dice rolled: "+d1.getFaceValue() + " and " + d2.getFaceValue());
+
+
+            System.out.println("Points after roll for player two: "+p2.getPoints());
+
+        }
+        System.out.println();
+        System.out.println("TIME TO FIND OUT WHO BITES THE DUST BITCHES");
+        System.out.println("Player one has: " + p1.getPoints() + " point and player two has: " + p2.getPoints() + " points");
+        System.out.println("WHICH FUCKING MEANS!!!!!");
+        if (p1.getPoints()>p2.getPoints()){
+            System.out.println("Player one OWNS player two SUCKA");
+        } else if (p1.getPoints()<p2.getPoints()){
+            System.out.println("Player two PWNS player one SUCKA");
+        } else {
+            System.out.println("Player one and player two are equals... pussies");
+        }
 
     }
 }
