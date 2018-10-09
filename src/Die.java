e79f2da6a3dbb5c3d316e39efb741a88afd5d270
JRJ86 <42966158+JRJ86@users.noreply.github.com>
2018-10-09 17:56:48 +0200

    Added constructor and roll method

--- src/Die.java
+++ src/Die.java
@@ -1,2 +1,19 @@
+import java.util.Random;
 public class Die {
+
+    int faceValue;
+
+    //Die constructor.
+    public Die()
+    {
+        faceValue = 1;
+    }
+    // Method used to roll a die.
+    public void roll()
+    {
+        Random rand = new Random();
+        faceValue = rand.nextInt(6)+1;
+    }
+
 }
+
