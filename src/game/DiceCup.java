0a37f530f37dc785a43601dea35422a5250a5f2f
Jacob <Jacob_ziska@hotmail.com>
2018-10-09 20:01:18 +0200

    Chua's message - made a dicecup

---
+++ src/game/DiceCup.java
@@ -0,0 +1,17 @@
+package game;
+
+public class DiceCup {
+    Die dice1 = new Die();
+    Die dice2 = new Die();
+    int totalValue;
+
+    public void roll(){
+        dice1.roll();
+        dice2.roll();
+        totalValue= dice1.getFaceValue()+dice2.getFaceValue();
+    }
+
+    public int getTotalValue() {
+        return totalValue;
+    }
+}
