51accec649823cfffc16381c72e51735207bf514
maltebp <maltebp94@gmail.com>
2018-10-10 20:38:19 +0200

    Malte: Created Test of probability of the different possible results when using DiceCup.roll(). + Deleted some unused test code

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -3,8 +3,6 @@ package test;
 import game.DiceCup;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -22,9 +20,9 @@ class GameTest {
 
         // Setup
         DiceCup diceCup = new DiceCup();
-        int die1Value = 0;
-        int die2Value = 0;
-        int dieTotalValue = 0;
+        int die1Value;
+        int die2Value;
+        int dieTotalValue;
 
         // Test
         for( int i=0; i< NUMBEROFROLLS; i++){
@@ -36,76 +34,46 @@ class GameTest {
         }
     }
 
-    /** Requirement 2.2
+    /** Requirement 2.2 - DiceCup Sum Probability Test
      *
      * @author Andreas and Malte
-     * Testing that the DiceCup works for 1000 rolls, and gives the right total value back.
+     * Testing that the probability of the different totalValues (2, 3 ... 12)
+     * of the DiceCup are matching the the mathematical probability.
+     *
+     * Test numbers:
+     *  - It tests each value's occurence after NUMBEROFTHROWS throws
+     *  - The value is allow to deviate by +- ALLOWEDDEVIATION decimal percent
+     *  - Source for the mathematicla probability of values on a pair of dice: https://wizardofodds.com/gambling/dice/
      */
+    @Test
+    void DiceCupValueDistributionTest(){
 
-    //@Test
-    /*void DiceCupValueDistributionTest(){
         // Settings
-        final int NUMBEROFTHROWS = 10000;
-        final double ALLOWEDDEVIATION = 0.05; // The allowed deviation for values
-
-        // Setup - setting up some test numbers
-        int[] occurence = new int[11]; // 0=2, 10=12
-        int instancesOfEach = NUMBEROFTHROWS/12; // Number of times each value should appear
-        //int deviationNumber = (int) ((double)instancesOfEach*deviationPercent);
-        int highBound = instancesOfEach+deviationNumber;
-        int lowBound  = instancesOfEach-deviationNumber;
+        final int       NUMBEROFTHROWS      = 100000;
+        final double    ALLOWEDDEVIATION    = 0.05; // The allowed deviation from the intended probability of values
+        final double[]  PROBABILITIES       = {0.0278, 0.0556, 0.0833, 0.1111, 0.1389, 0.1667, 0.1389, 0.1111, 0.0833, 0.0556, 0.0278};
+            // list index 0 equals to the sum of values on the dice 2, and 10 equals to 12.
 
         // Setup
+        int[] occurences = new int[11]; // 0=2, 10=12
+        DiceCup diceCup = new DiceCup();
 
+        // Acting
+        for( int i=0; i<NUMBEROFTHROWS; i++) {
+            diceCup.roll();
+            int value = diceCup.getTotalValue();
+            occurences[value-2]++; }
 
-        // Test
-        for
+        // Checking results
+        for( int index=0; index<occurences.length; index++){
+            int occurence = occurences[index];
+            double value_exactExpectedOccurence = NUMBEROFTHROWS * PROBABILITIES[index];
+            int value_highBound = (int)(value_exactExpectedOccurence*(1+ALLOWEDDEVIATION));
+            int value_lowBound = (int)(value_exactExpectedOccurence * (1-ALLOWEDDEVIATION));
 
-        // Result Printing
+            // Prints the result to the console, including the intended highbound and lowbound values.
+            System.out.println("Value "+(index+2)+": "+"\t"+occurence+" \t("+value_lowBound+"-"+value_highBound+")");
+            assertTrue( occurence>= value_lowBound && occurence<=value_highBound); }
 
     }
-
-
-    void diceRatioTest(){
-
-        // Setting up test numbers
-        int numberOfThrows = 1000000;
-        double deviationPercent = 0.01; // The allowed deviation before the test fails
-        int instancesOfEach = numberOfThrows/6;
-        int deviationNumber = (int) ((double)instancesOfEach*deviationPercent);
-        int highBound = instancesOfEach+deviationNumber;
-        int lowBound  = instancesOfEach-deviationNumber;
-
-        // Setting up test variables
-        Die die = new Die();
-        ArrayList<Integer> valueInstanc = new ArrayList<Integer>();
-        valueInstanc.add(0);
-        valueInstanc.add(0);
-        valueInstanc.add(0);
-        valueInstanc.add(0);
-        valueInstanc.add(0);
-        valueInstanc.add(0);
-
-        // Running Tests
-        int dieValue = 0;
-        for(int i=0;i<numberOfThrows;i++){
-            dieValue = die.roll();
-            valueInstanc.set(dieValue-1, valueInstanc.get(dieValue-1)+1);
-        }
-
-        System.out.println("Face instances has to be between "+lowBound+" and  "+highBound);
-
-        // Printing results
-        int dieThrows = 0;
-        for(int i=0; i<6; i++){
-            dieThrows += valueInstanc.get(i);
-            System.out.println("Face "+(i+1)+" instances: "+valueInstanc.get(i));
-        }
-
-        // Checking tests
-        assertTrue(numberOfThrows==dieThrows);
-        for(int i=0; i<6; i++){
-            assertTrue(valueInstanc.get(i)>=lowBound && valueInstanc.get(i)<=highBound);
-        }*/
-    //}
 }
